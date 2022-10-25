a3cb54662138e46413d0e91e1b4637a34372a131
[AlenBiz] <[jobbizyaev@gmail.com]>
2022-10-25 16:07:35 +0200

    fix and second option

--- 5-5.js
+++ 5-5.js
@@ -1,7 +1,20 @@
+//1 вариант
+let value1 = 1;
+let rand = (Math.random() * 100);
+console.log(rand);
+
+let value2 = Math.floor(rand);
+console.log(value2);
+let result = value1 + value2 / 5 + 1;
+console.log(result);
+
+
+//2 варинат
 let value1 = 1;
 let value2 = Math.floor(Math.random() * 100);
 
 console.log(value2);
 
-let result = value1 + value2 / 5 + 1
+let result = value1 + value2 / 5 + 1;
+console.log(result);
 /*По приоритету выполнения:все как в математике, сначала умножение/деление, потом сложение */
--- task_score.js
+++ task_score.js
@@ -4,9 +4,3 @@ let js = 80;
 let gpa = (intro + git + js) / 3;
 console.log(gpa);
 
-let namee = "Alena Biziaeva";
-console.log(namee);
-
-let name1 = namee.slice(0,1);
-
-console.log(name1);
