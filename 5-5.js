18bb09944004bbbf9f7987190c5d2f7b3f5491b7
[AlenBiz] <[jobbizyaev@gmail.com]>
2022-10-25 08:05:19 +0200

    added a 5-5js

---
+++ 5-5.js
@@ -0,0 +1,7 @@
+let value1 = 1;
+let value2 = Math.floor(Math.random() * 100);
+
+console.log(value2);
+
+let result = value1 + value2 / 5 + 1
+/*По приоритету выполнения:все как в математике, сначала умножение/деление, потом сложение */
